4c09512214d25e1af629e364af75603bd23d269a
Erin K <erin@penguinstampede.com>
2017-03-18 23:53:48 -0700

    adding shipit and keys to travis

---
+++ shipitfile.js
@@ -0,0 +1,18 @@
+var path = require('path');
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/themer',
+      deployTo: '/opt/pengu/themer',
+      repositoryUrl: 'https://github.com/penguinstampede/structure-themer.git',
+      ignores: ['.git','config','test'],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      key: 'config/deploy_id_rsa',
+      shallowClone: true
+      servers: 'pinguino@themer.penguinstampede.com'
+    }
+  });
+};
